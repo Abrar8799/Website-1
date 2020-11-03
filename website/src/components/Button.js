f261da39be22415fa1b790663b210011beb6922a
Abrar8799 <abrarhussain8799@gmail.com>
2020-11-03 18:50:51 +0530

    Website-1

---
+++ website/src/components/Button.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import './Button.css';
+import { Link } from 'react-router-dom';
+
+const STYLE= ['btn--primary','btn--outline'];
+const SIZES = ['btn--medium','btn--large'];
+
+const Button =({
+
+    children,
+    type,
+    onClick,
+    ButtonStyle,
+    ButtonSize
+
+})=> {
+
+    const checkbuttonStyle =STYLE.includes(ButtonStyle) ? ButtonStyle:STYLE[0];
+
+    const checkButtonSize=SIZES.includes(ButtonSize)? ButtonSize :SIZES[0];
+
+    return(
+
+        <Link to='/Sign-up' className='btn-mobile'>
+
+            <button 
+            className={`btn ${ checkbuttonStyle} ${checkButtonSize}`}
+            onClick={onClick}
+            type={type}>
+            {children}
+                  </button>
+            
+            </Link>
+    )
+
+}
+
+export default Button
---
+++ website/src/components/Navbar.js
@@ -0,0 +1,69 @@
+import React , { useState ,useEffect}  from 'react';
+import './Navbar.css';
+import { Link } from 'react-router-dom';
+import Button  from './Button'
+
+const Navbar=()=> {
+
+    const [click,setclick]=useState(false);
+    const [button ,setbutton]=useState(true)
+
+    const handleclick=()=> {setclick(!click);}
+
+    const closemobileMenu=()=>{
+        setclick(false )
+    }
+
+    const showbutton =()=> {
+        if(window.innerWidth<= 960){
+            setbutton(false)
+        }
+        else{
+            setbutton(true);
+        }
+    }; 
+    useEffect(()=>{
+        showbutton();
+    },[]);
+
+    window.addEventListener('resize',showbutton) 
+   
+return( <>
+<nav className="navbar">
+    <div className="navbar-container">
+        <Link to="/" className="navbar-logo" onClick={closemobileMenu}>
+         H&F.com<i className="fas fa-tshirt"/>
+         <br></br>
+         </Link>
+        
+        <div className="menu-icon" onClick={handleclick}>
+            <i className= { click ? 'fas fa-times' : 'fas fa-bars'}/>
+        </div>
+
+        <ul className={click ? 'nav-menu active':'nav-menu'}>
+            <li className='nav-item'>
+                <Link to='Home/' 
+                className='nav-links'
+                 onClick={closemobileMenu}>Home</Link>
+            </li>
+            <li className='nav-item'>
+                <Link to='/Products'
+                 className='nav-links'
+                  onClick={closemobileMenu}>Products</Link>
+            </li>
+            <li className='nav-item'>
+                <Link to='/Services'
+                 className='nav-links' 
+                 onClick={closemobileMenu}>Services</Link>
+            </li>
+            <li className='nav-item'>
+                <Link to='/ Sign-up' className='nav-links' onClick={closemobileMenu}>Sign-up</Link>
+            </li>
+            </ul>
+            {button && <Button ButtonStyle='btn--outline'> Sign-up </Button>}
+        </div>
+</nav>
+</> 
+ )
+}
+export default  Navbar
